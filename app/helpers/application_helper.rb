331be6605cd998b75c9efdbca0e305ac65132c0d
SereneMatt <serene1990@gmail.com>
2021-05-04 00:16:25 +0200

    [Login and session] Create users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def current_user
+    # Memoize to avoid hitting database for every time a user session is checked
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
